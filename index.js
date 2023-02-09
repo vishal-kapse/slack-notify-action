5b5d2caba1bd02796c196fcd1bf1fc63bb04fe50
vishal-kapse <vishal.kapse@cldcvr.com>
2023-02-09 12:10:30 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const request = require('request');
 
+try{
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+  const webhook = core.getInput('SLACK_WEBHOOK_URL');
+  const color = core.getInput('color');
+  const message = core.getInput('message');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+  core.debug(`webhook url supplied ${webhook}`);
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  const payload = JSON.stringify({
+    attachments: [
+      {
+        fallback: 'Github Actions Run Information',
+        color: `${color}`,
+        pretext: `${message}`,
+        fields: [
+          {
+            title: 'Repository',
+            value: process.env.GITHUB_REPOSITORY,
+            short: true,
+          },
+          {
+            title: 'Workflow',
+            value: `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+            short: true,
+          },
+          {
+            title: 'Run ID',
+            value: process.env.GITHUB_RUN_ID,
+            short: true,
+          },
+          {
+            title: 'Actor',
+            value: process.env.GITHUB_ACTOR,
+            short: true,
+          },
+        ],
+      },
+    ],
+  });
+
+  const options = {
+    url: `${webhook}`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: payload,
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return;
+    }
+
+    console.log(`Status code: ${response.statusCode}`);
+    console.log(`Response body: ${body}`);
+  });
+
+}catch (error){
+  core.error(error.message)
+  core.setFailed(error.message)
 }
-
-run();
